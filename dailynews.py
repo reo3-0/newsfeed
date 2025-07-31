b77e26f43624a8402a9a0cbeee494d83d5311aba
Ruairi OCearuil <81722958+reo3-0@users.noreply.github.com>
2025-07-31 10:40:07 -0400

    Update dailynews.py with another print test statement

--- dailynews.py
+++ dailynews.py
@@ -65,6 +65,7 @@ def archive_results(article_list_of_lists):
 
     # Turn the list of lists into a dataframe with uniform column naming 
     df_these_articles = pd.DataFrame(article_list_of_lists).rename(columns={0: "Publication Datetime", 1: "Headline", 2: "URL", 3: "Source"}) 
+    print(df_these_articles)
     df_these_articles['Publication Datetime'] = pd.to_datetime(df_these_articles['Publication Datetime']) #, utc=True , format='%y%m%d'
     df_these_articles.sort_values(by=['Publication Datetime'], ascending=False, inplace=True)
     df_these_articles['Publication Date'] = df_these_articles['Publication Datetime'].dt.date
